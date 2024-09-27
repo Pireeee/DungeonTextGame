f3a378ade88ce43b0fc6c9dc56cd712387ca523e
Raf <raphael.choquet@gmail.com>
2024-09-27 18:47:16 +0200

    items + rework stats

--- src/main/kotlin/entities/Player.kt
+++ src/main/kotlin/entities/Player.kt
@@ -1,13 +1,21 @@
 package entities
 
+import fr.entities.Item
+
 data class Player(
     val name: String,
     val playerClass: PlayerClass,
-    var strength: Int,
-    var defense: Int,
-    var health: Int,
-    var mana: Int,
+    var baseStrength: Int,
+    var baseDefense: Int,
+    var baseHealth: Int,
+    var baseMana: Int,
+    var totalStrength: Int = baseStrength,
+    var totalDefense: Int = baseDefense,
+    var totalHealth: Int = baseHealth,
+    var totalMana: Int = baseMana,
+    var inventory: MutableList<Item> = mutableListOf()
 )
+
 
 enum class PlayerClass {
     WARRIOR,
