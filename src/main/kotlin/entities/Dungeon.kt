73bee9450d6d739f336b61dec094ad1f14f2ecd4
Pire <p.clement01@orange.fr>
2024-10-25 14:36:17 +0200

    modify dungeon class

--- src/main/kotlin/entities/Dungeon.kt
+++ src/main/kotlin/entities/Dungeon.kt
@@ -1,5 +1,30 @@
 package entities
 
-data class Dungeon (
-    val rooms: List<Room>
-)
+data class Dungeon(
+    val width: Int,
+    val height: Int,
+    val grid: Array<Array<Cell>>
+){
+    init {
+        require(width > 0 && height > 0) { "Dungeon dimensions must be positive" }
+    }
+
+    fun getCell(x: Int, y:Int): Cell? {
+        return if (x in 0 until width && y in 0 until height) {
+            grid[x][y]
+        } else {
+            null
+        }
+    }
+    fun setCell(x: Int, y: Int, cell: Cell) {
+        if (x in 0 until width && y in 0 until height) {
+            grid[x][y] = cell
+        }
+    }
+}
+
+sealed class Cell {
+    object Empty : Cell()
+    data class MonsterCell(val monster: Monster) : Cell()
+    data class Treasure(val description: String, val value: Int) : Cell()
+}
